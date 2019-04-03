59b08e71ea186011863ea69de2fde27ab1fd3351
logunlaja26 <logunlaja26@gmail.com>
2019-04-03 15:48:37 -0500

    added templates Folder

--- randomSongApp/views.py
+++ randomSongApp/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is the homepage")
+    #return HttpResponse("This is the homepage")
+    return render(request,'randomSongApp/home.html')
