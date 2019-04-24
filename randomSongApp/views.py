f81e39e94abf8ea5145645149269398b91b5de6d
logunlaja26 <logunlaja26@gmail.com>
2019-04-23 20:25:22 -0500

    Bearer token api buildout

--- randomSongApp/views.py
+++ randomSongApp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from randomSongApp.forms import searchForm
 
-# Create your views here.
+
+
 
 def index(request):
     return render(request,'randomSongApp/home.html')
 
 def searchFormView(request):
     form = searchForm()
-    return render(request,'randomSongApp/searchpage.html',{'form':form})
+    if request.method == "POST":
+        url = 'https://accounts.spotify.com/api/token'
+        payload = {'client_id': '2f9fe9c1bb0f44fbaa27e0b1addc958d' ,'client_secret': 'dba5b3924f5f4ee2a6b582a0a70ad270'}
+        headers = {
+            'Authorization': 'Basic {{client_credentials}}'
+        }
+        r = requests.post(url, headers = headers, data=payload)
+        print(r.text)
+    #return render(request,'randomSongApp/searchpage.html',{'form':form})
