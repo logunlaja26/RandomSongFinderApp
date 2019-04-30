9c727eb3925a829fb71d43d8c49c7b79637b9e82
logunlaja26 <logunlaja26@gmail.com>
2019-04-30 15:53:29 -0500

    Added grant type to views.py

--- randomSongApp/views.py
+++ randomSongApp/views.py
@@ -12,10 +12,7 @@ def searchFormView(request):
     form = searchForm()
     if request.method == "POST":
         url = 'https://accounts.spotify.com/api/token'
-        payload = {'client_id': '2f9fe9c1bb0f44fbaa27e0b1addc958d' ,'client_secret': 'dba5b3924f5f4ee2a6b582a0a70ad270'}
-        headers = {
-            'Authorization': 'Basic {{client_credentials}}'
-        }
-        r = requests.post(url, headers = headers, data=payload)
+        payload = {'grant_type':'client_credentials','client_id': '2f9fe9c1bb0f44fbaa27e0b1addc958d' ,'client_secret': 'dba5b3924f5f4ee2a6b582a0a70ad270'}
+        r = requests.post(url, data=payload)
         print(r.text)
-    #return render(request,'randomSongApp/searchpage.html',{'form':form})
+    return render(request,'randomSongApp/searchpage.html',{'form':form})
