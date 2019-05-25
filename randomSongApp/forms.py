85ff34154ca96a566b631f7d7794db9f5c8ce6ef
logunlaja26 <logunlaja26@gmail.com>
2019-05-25 09:19:50 -0500

    Added radio buttons in search page

--- randomSongApp/forms.py
+++ randomSongApp/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
 
+select_choice = [
+    ('artist','Artist'),
+    ('album','Album'),
+    ('track','Track'),
+]
+
 class searchForm(forms.Form):
-    Artist = forms.CharField()
+    Artist_Info = forms.CharField()
     #Album = forms.CharField()
     #Song = forms.CharField()
+    selected_radio = forms.CharField(label='Make your selection',widget=forms.RadioSelect(choices=select_choice))
--- randomSongApp/views.py
+++ randomSongApp/views.py
@@ -27,6 +27,7 @@ def getArtist(request):
         if not form.is_valid():
             return render(request,'randomSongApp/searchpage.html',{'form': searchForm()})
         url_safe_artist = request.POST["Artist"]
+        #url_safe_artist = request.POST("select_choice")
         url = f'https://api.spotify.com/v1/search?q={url_safe_artist}&type=artist'
         headers = {
             'Accept': 'application/json',
@@ -41,8 +42,24 @@ def getArtist(request):
             'artist_popularity': (results[0]["popularity"])
         }
 
-        pprint(results)
-        print(type(results))
-        #print(results[0]["name"])
-        #print(results[0]["followers"]["total"])
+        #pprint(results)
+        #print(type(results))
     return render(request,'randomSongApp/searchpage.html',{'form':searchForm(),'context':context})
+
+    def getAlbum(request):
+        if request.method == "POST":
+            auth_url = 'https://accounts.spotify.com/api/token'
+            payload = {'grant_type':'client_credentials','client_id': '2f9fe9c1bb0f44fbaa27e0b1addc958d' ,'client_secret': 'dba5b3924f5f4ee2a6b582a0a70ad270'}
+            r = requests.post(auth_url, data=payload)
+            access_token = (r.json())['access_token']
+
+            form = searchForm(request.POST)
+            if not form.is_valid():
+                return render(request,'randomSongApp/searchpage.html',{'form': searchForm()})
+
+            url = 'https://api.spotify.com/v1/search?q=The+Massacre&type=artist'
+            headers = {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer %s' %access_token
+            }
+            print('-- Headers %s--'%headers)
